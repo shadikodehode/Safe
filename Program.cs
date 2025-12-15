5da1ad1908d7a4c92511c7ca90176f6894d84780
shadikodehode <shadi.kodehode@gmail.com>
2025-12-15 22:08:12 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Safe
+{
+    public static void Main()
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader (@"crack.txt"))
+            {
+                string? line;
+                        
+                while ((line = sr.ReadLine()) != null)
+                {
+                    
+                  if(String.IsNullOrEmpty("R"))
+                    {
+                        Console.WriteLine("success");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("the file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
+
+
+
